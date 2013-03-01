0a46f4285c9f87e66c9f5366fe1a05fae8950966
P S Saravanan <saravanp@thoughtworks.com>
2013-03-01 22:45:05 +0530

    Saravanan initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sweethome::Application.config.secret_key_base = 'e4895f81cd9dbecf057ec4b1061a91779fe10456a3900c09580bf15fcbbfd6719681cd1378e6d41bf60638edc74b25cef375e8112e792e2069ff94c456a8f14f'
